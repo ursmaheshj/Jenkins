ebb555b803547261ce022740223109a076133770
Mahesh Jadhav <58790433+ursmaheshj@users.noreply.github.com>
2022-06-25 16:34:38 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+// Print Your Name 
+public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will print 'your name' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello Mahesh"); // prints atnyla.com
+   }
+}
